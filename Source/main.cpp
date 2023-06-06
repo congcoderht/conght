85198937d2b68c623669c505b5ec71c84cb6d86a
congcoderht <vietcongtruong07@gmail.com>
2023-06-06 21:38:58 +0700

    up date

--- Source/main.cpp
+++ Source/main.cpp
@@ -3,20 +3,36 @@ using namespace std;
 int main()
 {
 	int n = 0;
-	int index = 0;
+	int index = -1;
 	int year = 0;
 	hocsinh* S;
+	Giaovien* T;
+	int m = 0;
 	Docfilehs(S, n);
-	cout << "----------Dang Nhap-------------" << endl;
-	string id, pass;
-	login(id, pass);
-	if (checkPassIDhs(S, id, pass, n,index))
-		cout << "Dang nhap thanh cong" << endl;
-	else
-		cout << "dang nhap sai" << endl;
-	cout << "Doi mat khau" << endl;
-	changepasshs(S, n, id);
-	capnhatfilehs(S, n);
-	taonamhoc(year);
+	Docfilegv(T, m);
+	//xuatHS(S, n);
+	//xuatGV(T, m);
+	do {
+		system("cls");
+		cout << "----------Dang Nhap-------------" << endl;
+		string id, pass;
+		login(id, pass);
+		if (checkPassIDhs(S, id, pass, n, index))
+		{
+			cout << "Dang nhap thanh cong" << endl;
+			cout << " --------------------------------------" << endl;
+			MenuHS(S, index, n, id);
+		}
+		else
+		{
+			index = -1;
+			cout << "dang nhap sai" << endl;
+		}
+	} while (index == -1);
+
+	//	cout << "Doi mat khau" << endl;
+		//changepasshs(S, n, id);
+		//capnhatfilehs(S, n);
+		//taonamhoc(year);
 	return 0;
 }
