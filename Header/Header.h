ccd82d1de1a53c5a8117459a3a582413bfcf0b3c
congcoderht <vietcongtruong07@gmail.com>
2023-06-06 21:38:26 +0700

    up date

--- Header/Header.h
+++ Header/Header.h
@@ -4,6 +4,10 @@
 #include<string>
 
 using namespace std;
+struct ngaythang {
+	int ngay;
+	int thang;
+};
 struct hocsinh {
 	string ID;
 	string ho;
@@ -25,22 +29,29 @@ struct Giaovien {
 struct khoahoc {
 	int tinchi;
 	int soluong;
-	int ngaybatdau;
-	int thangbatdau;
-	int ngayketthuc;
-	int thangketthuc;
+	ngaythang batdau;
+	ngaythang ketthuc;
 	string id;
 	string tenkhoa;
+	string tenGiaovien;
 	string tenlop;
 	string ngay;
 	string buoi;
 };
 int demHocsinh();
-void Docfilehs(hocsinh*& S,int &n);
-bool checkPassIDhs(hocsinh* S, string id, string pass,int n,int &index);
-void xuat1hs(hocsinh* S, string id,int n);
-void xuatHS(hocsinh* S,int n);
+int demgiaovien();
+void Docfilehs(hocsinh*& S, int& n);
+void Docfilegv(Giaovien*& T, int& m);
+bool checkPassIDhs(hocsinh* S, string id, string pass, int n, int& index);
+bool checkPassIDgv(Giaovien* T, string id, string pass, int m, int& index2);
+void xuat1hs(hocsinh S);
+void xuatHS(hocsinh* S, int n);
+void xuat1GV();
+void xuatGV(Giaovien* T, int m);
 void login(string& id, string& pass);
+void MenuHS(hocsinh* S, int& index, int n, string id);
+void MenuHeThong();
 void changepasshs(hocsinh*& S, int n, string id);
-void capnhatfilehs(hocsinh* &S, int n);
+void capnhatfilehs(hocsinh*& S, int n);
 void taonamhoc(int& newyear);
+void themlophocvaonam1();
