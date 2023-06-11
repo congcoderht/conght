b7f13e36a41ae60140380259d11fe230fdd2dfd9
congcoderht <vietcongtruong07@gmail.com>
2023-06-11 21:58:39 +0700

    New Update

--- Source/main.cpp
+++ Source/main.cpp
@@ -2,26 +2,29 @@
 using namespace std;
 int main()
 {
-	int n = 0;
+	int n = 0;int m = 0;
 	int index = -1;
 	int year = 0;
-	hocsinh* S;
-	Giaovien* T;
-	int m = 0;
-	Docfilehs(S, n);
-	Docfilegv(T, m);
-	//xuatHS(S, n);
+	namhoc newyear;
+	hocsinh* S = NULL;
+	Giaovien* T = NULL;
+	Lophoc* C = NULL;
+	Khoahoc* K = NULL;
+	ReadStudent(S, n);
+	ReadTeacher(T, m);
+	/*PrintfStudent(S, n);
+	PrintfTeacher(T, m);*/
+	system("pause");
 	//xuatGV(T, m);
 	do {
 		system("cls");
-		cout << "----------Dang Nhap-------------" << endl;
 		string id, pass;
 		login(id, pass);
-		if (checkPassIDhs(S, id, pass, n, index))
+		if (CheckPassIDStudent(S, id, pass, n, index))
 		{
-			cout << "Dang nhap thanh cong" << endl;
+			cout << "           Dang nhap thanh cong" << endl;
 			cout << " --------------------------------------" << endl;
-			MenuHS(S, index, n, id);
+			MenuStudent(S, index, n, id);
 		}
 		else
 		{
